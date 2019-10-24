e4c5958d83283dc51796b17b538cfc1b3da1cdd2
zhaozhengkang <zhaozhengkang@cetiti.com>
2019-10-24 16:53:12 +0800

    feat: add actuator sub-project
    新增子项目，actuator

---
+++ spring-boot-demo-actuator/src/test/java/com/pd/actuator/SpringBootDemoActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
