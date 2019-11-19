1109367bbb227371288617f4c76e06930ff20f34
zhaozhengkang <zhaozhengkang@cetiti.com>
2019-11-19 16:33:14 +0800

    feat: add swagger starter using instance
    使用swagger-spring-boot-starter配置文档并添加示例

---
+++ spring-boot-demo-swagger-battcn/src/main/java/com/pd/swagger/SpringBootDemoSwaggerBattcnApplication.java
@@ -0,0 +1,12 @@
+package com.pd.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoSwaggerBattcnApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoSwaggerBattcnApplication.class, args);
+    }
+
+}
