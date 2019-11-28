24e8092a56cf658fbfac82a009bfa2eb39a2cdea
zhaozhengkang <zhaozhengkang@cetiti.com>
2019-11-28 09:43:27 +0800

    feat: 新增待学模块 zookeeper

---
+++ spring-boot-demo-zookeeper/src/main/java/com/pd/zookeeper/SpringBootDemoZookeeperApplication.java
@@ -0,0 +1,13 @@
+package com.pd.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoZookeeperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoZookeeperApplication.class, args);
+    }
+
+}
---
+++ spring-boot-demo-zookeeper/src/test/java/com/pd/zookeeper/SpringBootDemoZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoZookeeperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
