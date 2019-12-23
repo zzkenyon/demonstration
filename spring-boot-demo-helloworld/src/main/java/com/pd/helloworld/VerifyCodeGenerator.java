dc9726dca16cf31b4edb71d874e49208e7e6b095
zhaozhengkang <zhaozhengkang@cetiti.com>
2019-12-23 20:00:49 +0800

    feat: context refresh 验证

--- spring-boot-demo-helloworld/src/main/java/com/pd/helloworld/VerifyCodeGenerator.java
+++ spring-boot-demo-helloworld/src/main/java/com/pd/helloworld/VerifyCodeGenerator.java
@@ -7,7 +7,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.security.SecureRandom;
-import java.util.Random;
 
 /**
  * @author zhaozhengkang
---
+++ spring-boot-demo-helloworld/src/main/java/com/pd/helloworld/context/TestConfig.java
@@ -0,0 +1,12 @@
+package com.pd.helloworld.context;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhaozhengkang
+ * @description
+ * @date 2019/12/23 16:11
+ */
+@Configuration
+public class TestConfig {
+}
